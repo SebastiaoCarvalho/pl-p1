2f80acf78320af927fc45b7774efb83cea89904c
Vlogi <luis200202@hotmail.com>
2024-04-29 11:01:20 +0100

    style(Task1): remove TODO comments

--- Imp.v
+++ Imp.v
@@ -164,9 +164,6 @@ Proof. reflexivity. Qed.
 (** Here is the formal definition of the abstract syntax of
     commands: *)
 
-(** 
-  1.1. TODO: Extend the datatype with the two new constructs as specified.
-*)
 Inductive com : Type :=
   | CSkip
   | CAsgn (x : string) (a : aexp)
@@ -179,10 +176,6 @@ Inductive com : Type :=
 (** As for expressions, we can use a few [Notation] declarations to
     make reading and writing Imp programs more convenient. *)
 
-
-(**
-  1.2. TODO: Define notations for the new constructs as required
-*)
 Notation "'skip'"  :=
          CSkip (in custom com at level 0) : com_scope.
 Notation "x := y"  :=
@@ -206,13 +199,5 @@ Notation "x -> y" :=
          (CCGuard x y)
             (in custom com at level 99, no associativity) : com_scope.
 
-(**
-  1.3. TODO: Define p1 and p2 as, respectively, the programs:
-
-                (X := 1  !!  X := 2);  X=2 -> skip
-             and
-                X:=2
-
-*)
 Example p1 := <{ (X := 1  !!  X := 2); (X=2 -> skip) }>.
 Example p2 := <{ X := 2 }>.
